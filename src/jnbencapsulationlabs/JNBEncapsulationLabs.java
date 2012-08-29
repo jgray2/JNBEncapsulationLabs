d3ae7aac42e956c4832760eb0b7958901bfb73d9
jgray2 <jgray2@L11705038603.wctc.msft>
2012-08-29 18:07:55 -0500

    first commit

---
+++ src/jnbencapsulationlabs/JNBEncapsulationLabs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jnbencapsulationlabs;
+
+/**
+ *
+ * @author jgray2
+ */
+public class JNBEncapsulationLabs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/lab1/Employee.java
@@ -0,0 +1,130 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * In this lab your challenge is to fix the code in both classes to use
+ * proper encapsulation and the four other best practices as explained by 
+ * your instructor.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.01
+ */
+public class Employee {
+//    String firstName;
+//    String lastName;
+//    public String ssn;
+//    public Date birthDate;
+//    boolean metWithHr;
+//    boolean metDeptStaff;
+//    boolean reviewedDeptPolicies;
+//    boolean movedIn;
+//    String cubeId;
+    
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private Date birthDate;
+    boolean metWithHr;
+    boolean metDeptStaff;
+    boolean reviewedDeptPolicies;
+    boolean movedIn;
+    private String cubeId;
+
+    public Employee() {
+
+    }
+//  add getters & setters for names, ssn, birthdate, cubeId
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    // create method to encompass first day activities
+    public void attendFirstDay() {
+        
+    }
+    // Assume this must be performed first
+    public void meetWithHrForBenefitAndSalryInfo() {
+        metWithHr = true;
+    }
+
+    // Assume this is must be performed second
+    public void meetDepartmentStaff() {
+        if(metWithHr) {
+            metDeptStaff = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot meet with "
+                    + "department staff until you have met with HR.");
+        }
+    }
+
+    // Assume this must be performed third
+    public void reviewDeptPolicies() {
+        if(metWithHr && metDeptStaff) {
+            reviewedDeptPolicies = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot review "
+                    + " department policies until you have first met with HR "
+                    + "and then with department staff.");
+        }
+    }
+
+    // Assume this must be performed 4th
+    public void moveIntoCubicle(String cubeId) {
+        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+            this.cubeId = cubeId;
+            this.movedIn = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot move in to a "
+                    + "cubicle until you have first met with HR "
+                    + "and then with department staff, and then reviewed"
+                    + "department policies.");
+        }
+
+    }
+
+    public String getStatus() {
+        if(metWithHr && metDeptStaff
+           && reviewedDeptPolicies && movedIn) {
+            return "Orientation is complete";
+        } else {
+            return "Orientation in progress...";
+        }
+    }
+}
